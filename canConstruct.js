80c26f287c5b2478290abc803cb32f6ea11e126e
khang <email@example.com>
2021-10-14 11:53:14 +0700

    14/10

--- canConstruct.js
+++ canConstruct.js
@@ -14,12 +14,39 @@ const canConstruct = (target, wordBank, memo = {}) => {
   return false;
 };
 
-console.log(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+// console.log(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+// console.log(
+//   canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
+// );
+// console.log(
+//   canConstruct(
+//     "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+//     ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"]
+//   )
+// );
+
+const canConstruct_tab = (target, wordBank) => {
+  const table = Array(target.length + 1).fill(false);
+  table[0] = true;
+  for (let i = 0; i <= target.length; i++) {
+    if (table[i] == true) {
+      for (let word of wordBank) {
+        //if the word matches the characters starting with position i
+        if (target.slice(i, i + word.length) === word) {
+          table[i + word.length] = true;
+        }
+      }
+    }
+  }
+  return table[target.length];
+};
+
+console.log(canConstruct_tab("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
 console.log(
-  canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
+  canConstruct_tab("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
 );
 console.log(
-  canConstruct(
+  canConstruct_tab(
     "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
     ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"]
   )
